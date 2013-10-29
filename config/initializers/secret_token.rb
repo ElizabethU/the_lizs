2189e25b23cf3d0868b4e5bdddcc145d4a2f370b
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2013-10-29 11:12:01 -0700

    Added all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheLizs::Application.config.secret_key_base = '95732153f61437083aef6a045d9cff6c5d2190ed16f33d793c090e97663999ce3bfa91dd976f965f96624b96923a83f035a044c0012f942a72308816d106483e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TheLizs::Application.config.session_store :cookie_store, key: '_the_lizs_session'
